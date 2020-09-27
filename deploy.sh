e97805d9d7d8c13e416f73e357d1bbf154ba8b50
yuxguo <yuxuanguo1999@gmail.com>
2020-09-27 16:57:32 +0800

    Sun Sep 27 16:57:32 CST 2020

--- deploy.sh
+++ deploy.sh
@@ -2,29 +2,16 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
-hugo -t hugo-coder # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-coder
 
-# Add changes to git.
 git add *
-
-# Input git password
-password="no password input!"
-
-if [ -n "$1" ]; then
-	password="$1"
-fi
-
-# Commit changes.
-msg="update the site at $(date)"
-if [ -n "$2" ]; then
-	msg="${*:2}"
+msg="$(date)"
+if [ -n "$*" ]; then
+	msg="$*"
 fi
 
 printf "commit message is: \033[0;32m$msg\033[0m\n"
-printf "ssh password is: \033[0;32m$password\033[0m\n"
 
 git commit -m "$msg"
 
-# Push source and build repos.
 git push origin master
